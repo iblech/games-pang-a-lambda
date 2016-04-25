633a01c0ec8f6b0497baee3283cc9b184f428f17
Ivan Perez <ivan.perez@keera.co.uk>
2016-04-25 09:57:52 +0300

    Delays level

--- Code/Game.hs
+++ Code/Game.hs
@@ -151,6 +151,18 @@ objEnemies 0 =
 objEnemies 1 =
   [ splittingBall ballMedium "ballEnemy1" (width/4, 300)   (360, -350)
   , splittingBall ballMedium "ballEnemy2" (3*width/4, 300) (360, -350) ]
+objEnemies 2 =
+  map ballLeft [1..6] ++ map ballRight [1..6]
+ where baseL = 20
+       sep   = width / 20
+       baseR = width - (baseL  + 6 * sep)
+
+       ballLeft n = splittingBall ballSmall ("ballEnemyL" ++ show n)
+                         (baseL + n * sep, 100) (-200, -200)
+
+       ballRight n = splittingBall ballSmall ("ballEnemyR" ++ show n)
+                           (baseR + n * sep, 100) (200, -200)
+
 objEnemies n =
   [ splittingBall ballBig "ballEnemy1" (600, 300) (360, -350) ]
 
