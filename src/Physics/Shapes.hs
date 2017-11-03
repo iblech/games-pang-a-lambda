a0ad68f6e680c5289d07b8d9e1b1579b20d59f3b
Ivan Perez <ivan.perez@keera.co.uk>
2017-11-03 13:26:22 +0100

    Cleans physics out.

--- src/Physics/Shapes.hs
+++
@@ -1,8 +0,0 @@
--- Copyright (c) 2011 - All rights reserved - Keera Studios
-module Physics.Shapes where
-
-import Physics.TwoDimensions.Dimensions
-
--- * Shapes
-type AABB   = (Pos2D, Size2D) -- We consider these half the size
-type Circle = (Pos2D, Double)
