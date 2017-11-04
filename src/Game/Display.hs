702a9b3327a3c6e06ca1f0aea1be49f954b54432
Ivan Perez <ivan.perez@keera.co.uk>
2017-11-05 00:21:32 +0100

    Moves resource manager out of display. Cleans code.

--- src/Game/Display.hs
+++ src/Game/Display.hs
@@ -1,27 +1,26 @@
 {-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
-module Game.Display where
+module Game.Display
+  ( module Game.Display
+  , module Game.ResourceManager
+  )
+  where
 
-import           Control.Arrow                   ((***))
 import           Control.Monad
 import           Control.Monad.IfElse
-import           Data.IORef
-import           Data.Maybe (fromJust)
+import           Data.Tuple.Extra
 import           FRP.Yampa.VectorSpace
 import           Graphics.UI.SDL                 as SDL
-import           Graphics.UI.SDL.Image           as SDL
-import qualified Graphics.UI.SDL.Primitives      as SDLP
 import qualified Graphics.UI.SDL.TTF             as TTF
-import           Graphics.UI.Extra.SDL           as SDL
 import           Graphics.UI.Extra.SDLDrawing    as SDL
 import           Graphics.UI.Extra.SDLPrimitives as SDL
 import           Text.Printf
 import           Physics.Shapes.BasicCirclesAABB
 
-import Data.Extra.List
 import Game.Constants
 import Game.GameState
 import Game.Objects
 import Game.Resources
+import Game.ResourceManager
 
 -- * Display handling
 
@@ -29,7 +28,7 @@ initializeDisplay :: IO ()
 initializeDisplay =
   SDL.init [InitEverything]
 
-initGraphs :: IORef Resources -> IO ()
+initGraphs :: ResourceManager -> IO ()
 initGraphs _res = void $ do
   screen <- SDL.setVideoMode (round width) (round height) 32 [SWSurface]
   SDL.setCaption gameName ""
@@ -43,15 +42,14 @@ initGraphs _res = void $ do
 
 -- * Rendering
 
-render :: IORef Resources -> GameState -> IO()
+render :: ResourceManager -> GameState -> IO()
 render resources shownState = do
   screen <- getVideoSurface
 
   -- Clear BG
   -- fillRect screen Nothing (Pixel backgroundColor)
   let lvl = gameLevel (gameInfo shownState)
-      safeBg l bgs = if l >= length bgs then last bgs else bgs !! l
-  bg <- (safeBg lvl . backgrounds) <$> readIORef resources
+  bg <- getBackgroundImage resources lvl
   blitSurface bg Nothing screen Nothing
 
   mapM_ (paintObject screen resources (gameTime (gameInfo shownState))) (gameObjects shownState)
@@ -69,7 +67,7 @@ render resources shownState = do
   SDL.flip screen
 
 -- ** Painting functions
-displayInfo :: Surface -> IORef Resources -> GameInfo -> Objects -> IO()
+displayInfo :: Surface -> ResourceManager -> GameInfo -> Objects -> IO()
 displayInfo screen resRef over objs = do
   msg <- printSolid resRef ("Time: " ++ printf "%.2f" (gameTime over))
   renderAlignRight screen msg (10,50)
@@ -77,50 +75,43 @@ displayInfo screen resRef over objs = do
     msg <- printSolid resRef ("Energy: " ++ show (playerEnergy p))
     renderAlignRight screen msg (10,100)
 
-paintObject :: Surface -> IORef Resources -> Double -> Object -> IO ()
+paintObject :: Surface -> ResourceManager -> Double -> Object -> IO ()
 paintObject screen resRef time object = do
-  resources <- readIORef resRef
+
   case objectProperties object of
-    (SideProps {}) -> return ()
-    (BallProps ballSize) -> void $ do
+
+    SideProps {} -> return ()
+
+    BallProps ballSize -> void $ do
       let (px,py)  = (\(u,v) -> (u, gameHeight - v)) (objectPos object)
-      let (x,y)    = (round *** round) (px,py)
+          (x,y)    = both round (px,py)
           (vx,vy)  = objectVel object
-          (x',y')  = (round *** round) ((px,py) ^+^ (0.1 *^ (vx, -vy)))
-      -- _ <- SDLP.filledCircle screen x y (round ballSize) (SDL.Pixel ballColor)
+          (x',y')  = both round ((px,py) ^+^ (0.1 *^ (vx, -vy)))
+
       ballImage <- getBallImage resRef (round ballSize)
+
       blitSurface ballImage Nothing screen (Just (Rect (fromIntegral x - round ballSize) (fromIntegral y - round ballSize) (-1) (-1)))
-      -- _ <- SDLP.line screen x y x' y' (SDL.Pixel velColor)
 
-      -- -- Print position
-      -- let font = miniFont resources
-      -- message <- TTF.renderTextSolid font (show $ (round *** round) (objectPos object)) fontColor
-      -- let w           = SDL.surfaceGetWidth  message
-      --     h           = SDL.surfaceGetHeight message
-      --     (x'',y'')   = (round *** round) (px,py)
-      --     rect        = SDL.Rect (x''+30) (y''-30) w h
-      -- SDL.blitSurface message Nothing screen (Just rect)
-      -- return ()
+    BlockProps sz@(w', h') -> void $ do
+      let (px,py)    = objectPos object
+          (x,y)      = both round (px, gameHeight - py - h')
 
-    (BlockProps sz@(w', h')) -> void $ do
-      let (px,py)  = (objectPos object)
-          (x,y)    = (round *** round) (px,gameHeight - py -h')
-          -- (w,h)    = (round *** round) sz
-          blockImage = if w' > h' then hblockImage else vblockImage
-      blockImg <- blockImage <$> readIORef resRef
+      let blockImage = if w' > h' then getHBlockImage else getVBlockImage
+      blockImg <- blockImage resRef
+
       blitSurface blockImg Nothing screen (Just (Rect x y (-1) (-1)))
-      -- fillRect screen (Just (Rect x y w h)) (Pixel blockColor)
 
-    (PlayerProps state _ vulnerable energy) -> do
+    PlayerProps state _ vulnerable energy -> do
       let blinkOn  = vulnerable || (even (round (time * 10)))
       when blinkOn $ void $ do
 
         let (px,py)  = (\(u,v) -> (u, gameHeight - v - playerHeight)) (objectPos object)
-        let (x,y)    = (round *** round) (px,py)
+            (x,y)    = both round (px,py)
             (vx,vy)  = objectVel object
-            (x',y')  = (round *** round) ((px,py) ^+^ (0.1 *^ (vx, -vy)))
-            (w,h)    = (round playerWidth, round playerHeight)
-            playerColor = case (state, vulnerable) of
+            (x',y')  = both round ((px,py) ^+^ (0.1 *^ (vx, -vy)))
+            (w,h)    = both round (playerWidth, playerHeight)
+
+        let playerColor = case (state, vulnerable) of
                             (PlayerShooting, _)     -> playerRightColor
                             (PlayerRight,    True)  -> playerRightColor
                             (PlayerLeft ,    True)  -> playerLeftColor
@@ -130,55 +121,40 @@ paintObject screen resRef time object = do
                             (PlayerStand,    False) -> playerBlinkStandColor
         playerImage <- getPlayerImage resRef state vulnerable
 
-        -- fillRect screen (Just (Rect x y w h)) (Pixel playerColor)
         blitSurface playerImage Nothing screen (Just (Rect x y (-1) (-1)))
 
-        -- _ <- SDLP.line screen (fromIntegral x) (fromIntegral y) x' y' (SDL.Pixel velColor)
-
-        -- -- Print position
-        -- let font = miniFont resources
-        -- message <- TTF.renderTextSolid font (show $ (round *** round) (objectPos object)) fontColor
-        -- let w           = SDL.surfaceGetWidth  message
-        --     h           = SDL.surfaceGetHeight message
-        --     (x'',y'')   = (round *** round) (px,py)
-        --     rect        = SDL.Rect (x''+30) (y''-30) w h
-        -- SDL.blitSurface message Nothing screen (Just rect)
-        -- return ()
-
     ProjectileProps -> void $ do
-        let (x0,y0)   = (\(x,y) -> (x - 5, height - y)) $ objectPos object
-            (dx, dy)  = (10, snd (objectPos object))
-            (x0', y0', dx', dy') = (round x0, round y0, round dx, round dy)
-        fillRect screen (Just (Rect x0' y0' dx' dy')) (Pixel bulletColor)
 
-paintShape :: Surface -> IORef Resources -> Double -> Object -> IO ()
+        let (x0, y0)  = both round $ (\(x,y) -> (x - 5, height - y)) $ objectPos object
+            (dx, dy)  = both round (10, snd (objectPos object))
+        fillRect screen (Just (Rect x0 y0 dx dy)) (Pixel bulletColor)
+
+paintShape :: Surface -> ResourceManager -> Double -> Object -> IO ()
 paintShape screen resources time object =
-  case objShape object of
-    Rectangle (px, py) (w,h) -> void $ do
-      let x1 = round px
-          x2 = round (px + w)
-          y1 = round (gameHeight - py - h)
-          y2 = round (gameHeight - py)
-      drawThickRectangle screen (Rect x1 y1 x2 y2) (Pixel collisionDebugColor) collisionDebugThickness
+  paintShape' screen resources time (objShape object)
 
-    Circle    (px, py) rd -> void $ do
-      let x = round px
-          y = round (gameHeight - py)
-          r = round rd
-      drawThickCircle screen x y r (Pixel collisionDebugColor) collisionDebugThickness
-
-    SemiPlane (px, py) s ->
-      let w = round width
-          h = round height
-      in case s of
-           LeftSide   -> drawThickLine screen 0 0 0 h (Pixel collisionDebugColor) collisionDebugThickness
-           RightSide  -> drawThickLine screen w 0 w h (Pixel collisionDebugColor) collisionDebugThickness
-           TopSide    -> drawThickLine screen 0 0 w 0 (Pixel collisionDebugColor) collisionDebugThickness
-           BottomSide -> drawThickLine screen 0 h w h (Pixel collisionDebugColor) collisionDebugThickness
-
+paintShape' :: Surface -> ResourceManager -> Double -> Shape -> IO ()
+paintShape' screen resources time (Rectangle (px, py) (w,h)) = void $
+    drawThickRectangle screen (Rect x1 y1 x2 y2) (Pixel collisionDebugColor) collisionDebugThickness
+  where
+    (x1, y1) = both round (px, gameHeight - py - h)
+    (x2, y2) = both round (px + w, gameHeight - py)
+paintShape' screen resources time (Circle (px, py) rd) = void $ do
+    drawThickCircle screen x y r (Pixel collisionDebugColor) collisionDebugThickness
+  where
+    (x, y) = both round (px, gameHeight - py)
+    r      = round rd
+paintShape' screen resources time (SemiPlane (px, py) s) =
+  case s of
+    LeftSide   -> drawThickLine screen 0 0 0 h (Pixel collisionDebugColor) collisionDebugThickness
+    RightSide  -> drawThickLine screen w 0 w h (Pixel collisionDebugColor) collisionDebugThickness
+    TopSide    -> drawThickLine screen 0 0 w 0 (Pixel collisionDebugColor) collisionDebugThickness
+    BottomSide -> drawThickLine screen 0 h w h (Pixel collisionDebugColor) collisionDebugThickness
+  where
+    (w,h) = both round (width, height)
 
 -- | Render Level message
-displayMessage :: Surface -> IORef Resources -> GameInfo -> IO()
+displayMessage :: Surface -> ResourceManager -> GameInfo -> IO()
 displayMessage screen resources info = case gameStatus info of
   GameLoading -> do
     msg <- printSolid resources ("Level " ++ show (gameLevel info))
@@ -186,114 +162,8 @@ displayMessage screen resources info = case gameStatus info of
   _ -> return ()
 
 -- * Auxiliary drawing functions
-printSolid :: IORef Resources -> String -> IO Surface
+printSolid :: ResourceManager -> String -> IO Surface
 printSolid resources msg = do
-  font    <- resFont <$> readIORef resources
+  font    <- getResFont resources
   message <- TTF.renderTextSolid font msg fontColor
   return message
-
--- * Resource handling
-
--- | Ad-hoc resource loading
--- This function is ad-hoc in two senses: first, because it
--- has the paths to the files hard-coded inside. And second,
--- because it loads the specific resources that are needed,
--- so it's not a general, parameterised, scalable solution.
---
-loadResources :: IO (IORef Resources)
-loadResources = do
-  -- Font initialization
-  _ <- TTF.init
-
-  -- Load the fonts we need
-  font            <- TTF.openFont "data/lacuna.ttf" 32 -- fixed size?
-  font2           <- TTF.openFont "data/lacuna.ttf" 8  -- fixed size?
-  backImages      <- mapM SDL.load [ "data/back.png" ]
-  hitImages       <- mapM SDL.load [ "data/hit0.png" ] -- , "data/hit2.png"]
-  walkLeftImages  <- mapM SDL.load [ "data/left-p3_walk01.png"
-                                   , "data/left-p3_walk02.png"
-                                   , "data/left-p3_walk03.png"
-                                   , "data/left-p3_walk04.png"
-                                   , "data/left-p3_walk05.png"
-                                   , "data/left-p3_walk06.png"
-                                   , "data/left-p3_walk07.png"
-                                   , "data/left-p3_walk08.png"
-                                   , "data/left-p3_walk09.png"
-                                   , "data/left-p3_walk10.png"
-                                   , "data/left-p3_walk11.png"
-                                   ]
-
-  walkRightImages <- mapM SDL.load [ "data/p3_walk01.png"
-                                   , "data/p3_walk02.png"
-                                   , "data/p3_walk03.png"
-                                   , "data/p3_walk04.png"
-                                   , "data/p3_walk05.png"
-                                   , "data/p3_walk06.png"
-                                   , "data/p3_walk07.png"
-                                   , "data/p3_walk08.png"
-                                   , "data/p3_walk09.png"
-                                   , "data/p3_walk10.png"
-                                   , "data/p3_walk11.png"
-                                   ]
-
-  standingImages  <- mapM SDL.load ["data/standing.png"]
-
-  ballImages100   <- mapM SDL.load ["data/ball1-200.png", "data/ball2-200.png"]
-  ballImages50    <- mapM SDL.load ["data/ball1-100.png", "data/ball2-100.png"]
-  ballImages25    <- mapM SDL.load ["data/ball1-50.png",  "data/ball2-50.png"]
-  ballImages12    <- mapM SDL.load ["data/ball1-25.png",  "data/ball2-25.png"]
-  hblockImg       <- SDL.load "data/hblock100.png"
-  vblockImg       <- SDL.load "data/vblock57.png"
-
-  backgrounds <- mapM SDL.load ["data/level0.png","data/level1.png","data/level2.png","data/level3.png"]
-
-  -- Return all resources (just the font)
-  newIORef $ Resources font font2 [ (PlayerVisualStand, (0, standingImages))
-                                  , (PlayerVisualRight, (0, walkRightImages))
-                                  , (PlayerVisualLeft,  (0, walkLeftImages))
-                                  , (PlayerVisualShoot, (0, backImages))
-                                  , (PlayerVisualHit,   (0, hitImages))
-                                  ]
-                                  [ (0, ballImages100)
-                                  , (0, ballImages50)
-                                  , (0, ballImages25)
-                                  , (0, ballImages12)
-                                  ]
-                                  hblockImg
-                                  vblockImg
-                                  backgrounds
-
-getBallImage :: IORef Resources -> Int -> IO Surface
-getBallImage resRef size = do
-  resources <- readIORef resRef
-  let ballIndex = case size of
-                    100 -> 0
-                    50  -> 1
-                    25  -> 2
-                    _   -> 3
-  let (n, imgs) = (ballImages resources) !! ballIndex
-      n' = if (n `div` 10) >= (length imgs - 1) then 0 else n + 1
-      newImages = updateL (ballImages resources) ballIndex (n', imgs)
-  writeIORef resRef (resources { ballImages = newImages })
-  return (imgs!!(n `div` 10))
-
-getPlayerImage :: IORef Resources -> PlayerState -> Bool -> IO Surface
-getPlayerImage resRef state vulnerable = do
-    resources <- readIORef resRef
-    let (n, imgs) = fromJust $ lookup visualState (playerImages resources)
-        n' = if (n `div` 2) >= (length imgs - 1) then 0 else (n+1)
-        updateN o@(s, (n, is)) = if s == visualState then (s, (n', is)) else o
-        newImages = map updateN (playerImages resources)
-    writeIORef resRef (resources { playerImages = newImages })
-    return (imgs!!(n `div` 2))
-  where 
-    visualState = case (state, vulnerable) of
-      (PlayerShooting, True)  -> PlayerVisualShoot
-      (PlayerRight,    True)  -> PlayerVisualRight
-      (PlayerLeft ,    True)  -> PlayerVisualLeft
-      (PlayerStand,    True)  -> PlayerVisualStand
-      (PlayerShooting, False) -> PlayerVisualShoot
-      (PlayerRight,    False) -> PlayerVisualRight
-      (PlayerLeft ,    False) -> PlayerVisualLeft
-      (PlayerStand,    False) -> PlayerVisualHit
-
---
+++ src/Game/ResourceManager.hs
@@ -0,0 +1,143 @@
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+module Game.ResourceManager where
+
+import           Control.Arrow                   ((***))
+import           Control.Monad
+import           Control.Monad.IfElse
+import           Data.IORef
+import           Data.Maybe (fromJust)
+import           FRP.Yampa.VectorSpace
+import           Graphics.UI.SDL                 as SDL
+import           Graphics.UI.SDL.Image           as SDL
+import qualified Graphics.UI.SDL.Primitives      as SDLP
+import qualified Graphics.UI.SDL.TTF             as TTF
+import           Graphics.UI.Extra.SDL           as SDL
+import           Graphics.UI.Extra.SDLDrawing    as SDL
+import           Graphics.UI.Extra.SDLPrimitives as SDL
+import           Text.Printf
+import           Physics.Shapes.BasicCirclesAABB
+
+import Data.Extra.List
+import Game.Constants
+import Game.GameState
+import Game.Objects
+import Game.Resources
+
+type ResourceManager = IORef Resources
+
+-- * Resource handling
+
+-- | Ad-hoc resource loading
+-- This function is ad-hoc in two senses: first, because it
+-- has the paths to the files hard-coded inside. And second,
+-- because it loads the specific resources that are needed,
+-- so it's not a general, parameterised, scalable solution.
+--
+loadResources :: IO (ResourceManager)
+loadResources = do
+  -- Font initialization
+  _ <- TTF.init
+
+  -- Load the fonts we need
+  font            <- TTF.openFont "data/lacuna.ttf" 32 -- fixed size?
+  font2           <- TTF.openFont "data/lacuna.ttf" 8  -- fixed size?
+  backImages      <- mapM SDL.load [ "data/back.png" ]
+  hitImages       <- mapM SDL.load [ "data/hit0.png" ] -- , "data/hit2.png"]
+  walkLeftImages  <- mapM SDL.load [ "data/left-p3_walk01.png"
+                                   , "data/left-p3_walk02.png"
+                                   , "data/left-p3_walk03.png"
+                                   , "data/left-p3_walk04.png"
+                                   , "data/left-p3_walk05.png"
+                                   , "data/left-p3_walk06.png"
+                                   , "data/left-p3_walk07.png"
+                                   , "data/left-p3_walk08.png"
+                                   , "data/left-p3_walk09.png"
+                                   , "data/left-p3_walk10.png"
+                                   , "data/left-p3_walk11.png"
+                                   ]
+
+  walkRightImages <- mapM SDL.load [ "data/p3_walk01.png"
+                                   , "data/p3_walk02.png"
+                                   , "data/p3_walk03.png"
+                                   , "data/p3_walk04.png"
+                                   , "data/p3_walk05.png"
+                                   , "data/p3_walk06.png"
+                                   , "data/p3_walk07.png"
+                                   , "data/p3_walk08.png"
+                                   , "data/p3_walk09.png"
+                                   , "data/p3_walk10.png"
+                                   , "data/p3_walk11.png"
+                                   ]
+
+  standingImages  <- mapM SDL.load ["data/standing.png"]
+
+  ballImages100   <- mapM SDL.load ["data/ball1-200.png", "data/ball2-200.png"]
+  ballImages50    <- mapM SDL.load ["data/ball1-100.png", "data/ball2-100.png"]
+  ballImages25    <- mapM SDL.load ["data/ball1-50.png",  "data/ball2-50.png"]
+  ballImages12    <- mapM SDL.load ["data/ball1-25.png",  "data/ball2-25.png"]
+  hblockImg       <- SDL.load "data/hblock100.png"
+  vblockImg       <- SDL.load "data/vblock57.png"
+
+  backgrounds <- mapM SDL.load ["data/level0.png","data/level1.png","data/level2.png","data/level3.png"]
+
+  -- Return all resources (just the font)
+  newIORef $ Resources font font2 [ (PlayerVisualStand, (0, standingImages))
+                                  , (PlayerVisualRight, (0, walkRightImages))
+                                  , (PlayerVisualLeft,  (0, walkLeftImages))
+                                  , (PlayerVisualShoot, (0, backImages))
+                                  , (PlayerVisualHit,   (0, hitImages))
+                                  ]
+                                  [ (0, ballImages100)
+                                  , (0, ballImages50)
+                                  , (0, ballImages25)
+                                  , (0, ballImages12)
+                                  ]
+                                  hblockImg
+                                  vblockImg
+                                  backgrounds
+
+getBallImage :: ResourceManager -> Int -> IO Surface
+getBallImage resRef size = do
+  resources <- readIORef resRef
+  let ballIndex = case size of
+                    100 -> 0
+                    50  -> 1
+                    25  -> 2
+                    _   -> 3
+  let (n, imgs) = (ballImages resources) !! ballIndex
+      n' = if (n `div` 10) >= (length imgs - 1) then 0 else n + 1
+      newImages = updateL (ballImages resources) ballIndex (n', imgs)
+  writeIORef resRef (resources { ballImages = newImages })
+  return (imgs!!(n `div` 10))
+
+getPlayerImage :: ResourceManager -> PlayerState -> Bool -> IO Surface
+getPlayerImage resRef state vulnerable = do
+    resources <- readIORef resRef
+    let (n, imgs) = fromJust $ lookup visualState (playerImages resources)
+        n' = if (n `div` 2) >= (length imgs - 1) then 0 else (n+1)
+        updateN o@(s, (n, is)) = if s == visualState then (s, (n', is)) else o
+        newImages = map updateN (playerImages resources)
+    writeIORef resRef (resources { playerImages = newImages })
+    return (imgs!!(n `div` 2))
+  where
+    visualState = case (state, vulnerable) of
+      (PlayerShooting, True)  -> PlayerVisualShoot
+      (PlayerRight,    True)  -> PlayerVisualRight
+      (PlayerLeft ,    True)  -> PlayerVisualLeft
+      (PlayerStand,    True)  -> PlayerVisualStand
+      (PlayerShooting, False) -> PlayerVisualShoot
+      (PlayerRight,    False) -> PlayerVisualRight
+      (PlayerLeft ,    False) -> PlayerVisualLeft
+      (PlayerStand,    False) -> PlayerVisualHit
+
+getBackgroundImage :: ResourceManager -> Int -> IO Surface
+getBackgroundImage resRef lvl = do
+  let safeBg l bgs = if l >= length bgs then last bgs else bgs !! l
+  bg <- (safeBg lvl . backgrounds) <$> readIORef resRef
+  return bg
+
+getResFont :: ResourceManager -> IO TTF.Font
+getResFont resRef = resFont <$> readIORef resRef
+
+getHBlockImage resRef = hblockImage <$> readIORef resRef
+getVBlockImage resRef = vblockImage <$> readIORef resRef
